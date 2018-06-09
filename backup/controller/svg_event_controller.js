38f1529ac5ec903f1fe66d600a013de44212d4d4
BESM2029 <40030552+BESM2029@users.noreply.github.com>
2018-06-09 22:31:27 +0900

    removing backup and node_modules

--- backup/controller/svg_event_controller.js
+++
@@ -1,9 +0,0 @@
-var g = new AG.AGraph("id", "__src", "__dst");
-    g.curr_id = 0;
-    g.addVtx(g.curr_id++, {tag:"rect", width:"300", height:"100", style:"fill:rgb(255,0,0);stroke-width:3;stroke:rgb(0,0,0)"});
-var rectangle_maker = new rectangle_creator(100, 100, g);
-var circle_maker = new circle_creator(100, g);
-function svg_onmousedown(e) {
-    rectangle_maker.onmousedown(e);
-    document.getElementById("output").innerHTML = render(g, 900, 900);
-}
